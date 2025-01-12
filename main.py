cf65c2ef8c8fdff7eccb582e6fd8734f3a3ecc73
gadhalevy <gad3dh@gmail.com>
2025-01-12 14:43:20 +0200

    Bold font convert styler to html. New algorithm that make numbers disapeare after all submitions. Change slider text appearance.
    Use only send link version, automatic creation with playwrite was suppressed

--- main.py
+++ main.py
@@ -1,45 +1,36 @@
-#This will not run on online IDE
-import streamlit as st
-import time
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
-from io import StringIO
+import os
+os.system("playwright install")
+os.system("playwright install-deps")
+import asyncio
+from playwright.async_api import async_playwright
+import requests,string,os
+from bs4 import BeautifulSoup
+import time,streamlit as st
 
-@st.cache_resource
-def init():
-    firefoxOptions = Options()
-    firefoxOptions.add_argument("--headless")
-    firefoxOptions.add_argument("--disable-gpu")
-    service = Service(GeckoDriverManager().install())
-    driver = webdriver.Firefox(
-        options=firefoxOptions,
-        service=service,
-    )
-    return  driver
-
-def get_url(words):
-    # driver=webdriver.Firefox()
-    driver=init()
-    driver.get('https://geek.co.il/~mooffie/crossword/')
-    form=driver.find_element(by='id',value='crossword-form')
-    area=form.find_element(by='id',value='raw-words')
-    area.clear()
-    area.send_keys(words)
-    submit=form.find_element(by='name',value='action_same')
-    driver.implicitly_wait(2)
-    submit.click()
-    driver.implicitly_wait(2)
-    form=driver.find_element(by='id',value='crossword-form')
-    # tbl=form.find_element(by='CLASS_NAME',value='control-panel')
-    driver.implicitly_wait(2)
-    link=form.find_element(by='id',value='save-create-temp-btn')
-    link.click()
-    url=driver.current_url
-    time.sleep(2)
-    driver.quit()
+async def run(playwright,words=None,url=None) -> None:
+    browser = await playwright.firefox.launch(headless=True)
+    context = await browser.new_context()
+    page = await context.new_page()
+    if url is None:
+        await page.goto("https://geek.co.il/~mooffie/crossword/")
+        await page.get_by_text("אנטיביוטיקה - תרופה המשמידה חיידקים קריקטורה - ציור הומוריסטי").press("ControlOrMeta+a")
+        await page.get_by_text("אנטיביוטיקה - תרופה המשמידה חיידקים קריקטורה - ציור הומוריסטי").fill(words)
+        await page.get_by_role("button", name="בנה את התשבץ").click()
+        await page.get_by_role("button", name="צוֹר קישור").click()
+    else:
+        await page.goto(url)
+        await page.get_by_role("button", name="ההצעה הבאה").click()
+        await page.get_by_role("button", name="צוֹר קישור").click()
+    url=page.url
+    # ---------------------
+    await context.close()
+    await browser.close()
     return url
+
+async def get_url(words=None,cond=None):
+    async with async_playwright() as playwright:
+        url = await run(playwright, words,cond)
+        return url
 
 def progress():
     progress_text = "Operation in progress. Please wait."
@@ -50,38 +41,62 @@ def progress():
     time.sleep(1)
     my_bar.empty()
 
-def set_session_state(url,length):
+
+@st.cache_data
+def set_session_state(url):
     if 'url' not in st.session_state:
         st.session_state.url=url
-    if 'length' not in st.session_state:
-        st.session_state.length=length
+    else:
+        st.session_state.url = url
 
-def main():
-    kovets=None
+async def get_msg(url):
+    r =  requests.get(url)
+    soup = BeautifulSoup(r.content,'html5lib')  # If this line causes an error, run 'pip install html5lib' or install html5lib
+    msg=soup.find('div',attrs={'class':"messages warning"}).get_text()
+    st.write(msg)
+    if 'הושמטו' in msg:
+        again=st.button('לא כל ההגדרות נוצרו אנא הרץ פעם נוספת')
+        if again:
+            await get_url(url)
+    st.write(msg)
+
+async def main():
+    '''
+    Run playwrite to make cross creation automatic.
+    Suppressed, have to create cross in advance, using send link option.
+    Uncomment code if you want to use the automatic version.
+
+    :return:
+    '''
     st.set_page_config(layout="wide")
-    ofen=st.sidebar.radio('בחר אופן יצירת התשבץ',('העלאת קובץ','יצירה במקום','קישור ישיר'))
+    kovets=None
+    ofen=st.sidebar.radio('בחר אופן יצירת התשבץ',('העלאת קובץ','יצירה במקום','קישור ישיר'),index=2)
     if ofen=='העלאת קובץ':
         tmp=st.file_uploader('העלה בבקשה את קובץ התשבץ')
         if tmp:
-            kovets=StringIO(tmp.getvalue().decode('utf-8'))
-            length=len(kovets.readlines())
+            kovets=tmp.getvalue().decode('utf-8')
+            set_session_state(kovets)
     elif ofen=='יצירה במקום':
         st.info('לאחר סיום כתיבת ההגדרות מומלץ לגבות אותן בקובץ לפני מעבר לשלב הבא')
         txt=st.text_area('כתוב הגדרות')
-        if st.sidebar.button('העלה'):
+        if st.sidebar.toggle('העלה'):
             kovets=txt
-            length=len(txt.split('\n'))
     else:
         url=st.text_input('כתוב קישור לתשבץ',value='https://geek.co.il/~mooffie/crossword/temporary/')
         if url:
             send_url=st.button('שלח קישור')
             if send_url:
-                set_session_state(url,length=0)
+                set_session_state(url)
+                st.info('!הקישור נשלח בהצלחה')
     if kovets is not None:
-        url=get_url(kovets)
+        url=await get_url(kovets)
+        await get_msg(url)
         progress()
-        st.write(url)
-        set_session_state(url, length)
+        if url:
+            set_session_state(url)
+            st.write(url)
 
 if __name__=='__main__':
-    main()
+    loop = asyncio.ProactorEventLoop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
