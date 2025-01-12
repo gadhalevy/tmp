ffa9483c7fa1843b3ca4c0c4c6a56094f8f1ff40
gadhalevy <gad3dh@gmail.com>
2025-01-12 17:23:27 +0200

    Bold font convert styler to html. New algorithm that make numbers disapeare after all submitions. Change slider text appearance.
    Use only send link version, automatic creation with playwrite was suppressed

--- pages/main_st.py
+++ pages/main_st.py
@@ -323,6 +323,10 @@ def main():
         page_title="פתור תשבץ",
         page_icon="🤔",
     )
+    st_pages.show_pages([
+        st_pages.Page("main.py", "צור תשבץ", "🏁"),
+        st_pages.Page("pages/main_st.py", "פתור תשבץ", "🤔"),
+    ])
     tab1, tab2 = st.tabs(['פתור תשבץ', 'דירוג משתמשים'])
     # Eliminate st.session_state url not declared bug.
     try:
