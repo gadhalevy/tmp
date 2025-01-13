125b9d83af9526ab1342b4c53bbef718d8b99bae
gadhalevy <gad3dh@gmail.com>
2025-01-13 15:01:59 +0200

    Bold font convert styler to html. New algorithm that make numbers disapeare after all submitions. Change slider text appearance.
    Use only send link version, automatic creation with playwrite was suppressed

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def run(playwright:Playwright,words=None,url=None) -> None:
     return url
 
 def get_url(words=None,cond=None):
+    st.write('cond=',cond)
     with sync_playwright() as playwright:
         url =  run(playwright, words,cond)
         return url
