15114aecd83462211bc61199e1369857baeb197e
gadhalevy <gad3dh@gmail.com>
2025-01-16 17:35:02 +0200

    Bold font convert styler to html. New algorithm that make numbers disapeare after all submitions. Change slider text appearance.
    Use only send link version, automatic creation with playwrite was suppressed

--- pages/main_st.py
+++ pages/main_st.py
@@ -386,7 +386,7 @@ def main():
             if col3.button('סיום',type='primary'):
                 show_res(col3)
                 st_lottie('https://lottie.host/ea1fa0a7-3547-458d-868e-b14f85b8d82d/rTfCy1CzPo.json',height=800,width=1200)
-                playsound('assets/sounds/victory.mp3')
+                st.audio('assets/sounds/victory.mp3',autoplay=True)
                 del_ref()
     # Complains about session_state.url
     except AttributeError:
